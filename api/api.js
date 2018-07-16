47d3bd910a3c91502bac1b7f87c205af9926bbdc
wwwung <wwwungj@163.com>
2018-07-16 22:24:53 +0800

    change

--- api/api.js
+++ api/api.js
@@ -142,8 +142,12 @@ let api = {
       console.log(chunk)
     })
     req.on('end', () => {
+      console.log(sessionId)
       const sessionId = cookieUtil.getSessionIdfromCookie(req.headers.cookie);
       if (req.headers.cookie && sessionId) {
+        if (user === null) {
+          res.end('未登录');
+        }
         const user = session.querySession(sessionId);
         res.end(JSON.stringify(user));
         return;
